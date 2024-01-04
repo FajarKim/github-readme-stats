042dc5b0d69ec29c711baafb2d71b982ea748246
Rangga Fajar Oktariansyah <86386385+FajarKim@users.noreply.github.com>
2024-01-04 13:36:46 +0700

    fixed error dor encode avatar url image to base64

--- src/common/icons.js
+++ src/common/icons.js
@@ -1,4 +1,4 @@
-import base64ImageFetcher from "node-base64-image";
+import fetch from "node-fetch";
 
 const icons = {
   star: `<path fill-rule="evenodd" d="M8 .25a.75.75 0 01.673.418l1.882 3.815 4.21.612a.75.75 0 01.416 1.279l-3.046 2.97.719 4.192a.75.75 0 01-1.088.791L8 12.347l-3.766 1.98a.75.75 0 01-1.088-.79l.72-4.194L.818 6.374a.75.75 0 01.416-1.28l4.21-.611L7.327.668A.75.75 0 018 .25zm0 2.445L6.615 5.5a.75.75 0 01-.564.41l-3.097.45 2.24 2.184a.75.75 0 01.216.664l-.528 3.084 2.769-1.456a.75.75 0 01.698 0l2.77 1.456-.53-3.084a.75.75 0 01.216-.664l2.24-2.183-3.096-.45a.75.75 0 01-.564-.41L8 2.694v.001z"/>`,
@@ -22,12 +22,18 @@ const icons = {
  * @param {string} avatarUrl - The avatar url github.
  * @returns {string} - The Base64 code.
  */
-const base64Image = async (avatarUrl) => {
-  const picture = await base64ImageFetcher.encode(avatarUrl, {
-    string: true,
+const base64Image = (avatarUrl) => {
+  return new Promise((resolve, reject) => {
+    fetch(avatarUrl)
+      .then(response => {
+        return response.arrayBuffer();
+      })
+      .then(buffer => {
+        const picture = Buffer.from(buffer).toString("base64");
+        resolve(picture);
+      });
   });
-  return picture;
-}
+};
 
 /**
  * Get rank icon
@@ -56,6 +62,19 @@ const rankIcon = (rankIcon, rankLevel, percentile, username) => {
       `;
     case "profile":
       const avatarUrl = `https://avatars.githubusercontent.com/${username}`;
+      return base64Image(avatarUrl)
+          .then(picture => `
+            <svg x="-38" y="-30" height="66" width="66" aria-hidden="true" viewBox="0 0 66 66" data-view-component="true" data-testid="github-rank-profile">
+              <defs>
+                <rect id="rect" x="0%" y="0%" width="100%" height="100%" rx="50"/>
+                <clipPath id="clip">
+                  <use href="#rect"/>
+                </clipPath>
+              </defs>
+              <image href="data:image/jpeg;base64,${picture}" width="100%" height="100%" clip-path="url(#clip)"/>
+            </svg>
+          `);
+/*
       const picture = base64Image(avatarUrl);
       return `
         <svg x="-38" y="-30" height="66" width="66" aria-hidden="true" viewBox="0 0 66 66" data-view-component="true" data-testid="github-rank-profile">
@@ -67,7 +86,7 @@ const rankIcon = (rankIcon, rankLevel, percentile, username) => {
           </defs>
           <image href="data:image/jpeg;base64,${picture}" width="100%" height="100%" clip-path="url(#clip)"/>
         </svg>
-      `;
+      `;*/
     case "default":
     default:
       return `
